177b6f2abaaeff39c16d22174247f860359bb498
Brenda Ombeyi <brendaotieno2001@gmail.com>
2024-11-19 12:24:02 +0300

    fundraiser

--- src/components/FundraiserCreated.js
+++ src/components/FundraiserCreated.js
@@ -107,7 +107,7 @@ const CreateFundraiser = ({ addFundraiser }) => {
         </div>
 
         <button
-          type="submit"
+          type='submit'
           className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700"
         >
           Create Fundraiser
